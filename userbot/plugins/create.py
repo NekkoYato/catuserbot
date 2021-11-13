7ae54425a0bb3d4068c6cd5c29ce0444377705fa
NekkoYato <89446086+NekkoYato@users.noreply.github.com>
2021-11-14 02:58:43 +0530

    Fuck it, YOLO!

--- userbot/plugins/create.py
+++ userbot/plugins/create.py
@@ -28,9 +28,9 @@ async def _(event):
     type_of_group = event.pattern_match.group(1)
     group_name = event.pattern_match.group(2)
     if type_of_group == "c":
-        descript = "This is a Test Channel created using catuserbot"
+        descript = "This is a Test Channel"
     else:
-        descript = "This is a Test Group created using catuserbot"
+        descript = "This is a Test Group"
     if type_of_group == "g":
         try:
             result = await event.client(
@@ -48,7 +48,8 @@ async def _(event):
                 )
             )
             await edit_or_reply(
-                event, f"Group `{group_name}` created successfully. Join {result.link}"
+                event,
+                f"Group [{group_name}]({result.link[8:]}) created successfully.",
             )
         except Exception as e:
             await edit_delete(event, f"**Error:**\n{str(e)}")
@@ -69,7 +70,7 @@ async def _(event):
             )
             await edit_or_reply(
                 event,
-                f"Channel `{group_name}` created successfully. Join {result.link}",
+                f"Channel [{group_name}]({result.link[8:]}) created successfully.",
             )
         except Exception as e:
             await edit_delete(event, f"**Error:**\n{e}")
@@ -80,7 +81,7 @@ async def _(event):
         if answer[0] != "error":
             await edit_or_reply(
                 event,
-                f"Mega group `{group_name}` created successfully. Join {answer[0].link}",
+                f"Mega group [{group_name}]({result.link[8:]}) created successfully.",
             )
         else:
             await edit_delete(event, f"**Error:**\n{answer[1]}")
