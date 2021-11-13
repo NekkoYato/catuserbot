98635e4f44ebf94912a01dbddba38531c1a843fc
NekkoYato <89446086+NekkoYato@users.noreply.github.com>
2021-11-14 00:44:24 +0530

    The universe is possible

---
+++ userbot/plugins/nhentai.py
@@ -0,0 +1,102 @@
+# By @kirito6969 for pepecat
+
+import re
+
+from hentai import Hentai, Utils
+from natsort import natsorted
+
+from userbot import catub
+
+from ..core.managers import edit_delete, edit_or_reply
+from ..helpers.tools import post_to_telegraph
+
+plugin_category = "useless"
+
+
+@catub.cat_cmd(
+    pattern="nhentai(?: |$)(.*)",
+    command=("nhentai", plugin_category),
+    info={
+        "header": "Some Cultured Stuff",
+        "description": "To find and read doujinsi from nhentai inside Telegram using telegraph",
+        "usage": [
+            "{tr}nhentai <link or code or 'random'>",
+        ],
+    },
+)
+async def _(event):
+    if event.fwd_from:
+        return
+    await edit_or_reply(event, "`Searching for doujin...`")
+    input_str = event.pattern_match.group(1)
+    code = input_str
+    if "nhentai" in input_str:
+        link_regex = r"(?:https?://)?(?:www\.)?nhentai\.net/g/(\d+)"
+        match = re.match(link_regex, input_str)
+        code = match.group(1)
+    if input_str == "random":
+        code = Utils.get_random_id()
+    try:
+        doujin = Hentai(code)
+    except BaseException as n_e:
+        if "404" in str(n_e):
+            return await edit_delete(
+                event, f"No doujin found for `{code}`. You shouldn't use nhentai :-("
+            )
+        return await edit_delete(event, f"**ERROR :** `{n_e}`")
+    msg = ""
+    imgs = "".join(f"<img src='{url}'/>" for url in doujin.image_urls)
+    imgs = f"&#8205; {imgs}"
+    title = doujin.title()
+    graph_link = await post_to_telegraph(title, imgs)
+    msg += f"[{title}]({graph_link})"
+    msg += f"\n**Source :**\n[{code}]({doujin.url})"
+    if doujin.parody:
+        msg += "\n**Parodies :**"
+        parodies = [
+            "#" + parody.name.replace(" ", "_").replace("-", "_")
+            for parody in doujin.parody
+        ]
+
+        msg += "\n" + " ".join(natsorted(parodies))
+    if doujin.character:
+        msg += "\n**Characters :**"
+        charas = [
+            "#" + chara.name.replace(" ", "_").replace("-", "_")
+            for chara in doujin.character
+        ]
+
+        msg += "\n" + " ".join(natsorted(charas))
+    if doujin.tag:
+        msg += "\n**Tags :**"
+        tags = [
+            "#" + tag.name.replace(" ", "_").replace("-", "_") for tag in doujin.tag
+        ]
+
+        msg += "\n" + " ".join(natsorted(tags))
+    if doujin.artist:
+        msg += "\n**Artists :**"
+        artists = [
+            "#" + artist.name.replace(" ", "_").replace("-", "_")
+            for artist in doujin.artist
+        ]
+
+        msg += "\n" + " ".join(natsorted(artists))
+    if doujin.language:
+        msg += "\n**Languages :**"
+        languages = [
+            "#" + language.name.replace(" ", "_").replace("-", "_")
+            for language in doujin.language
+        ]
+
+        msg += "\n" + " ".join(natsorted(languages))
+    if doujin.category:
+        msg += "\n**Categories :**"
+        categories = [
+            "#" + category.name.replace(" ", "_").replace("-", "_")
+            for category in doujin.category
+        ]
+
+        msg += "\n" + " ".join(natsorted(categories))
+    msg += f"\n**Pages :**\n{doujin.num_pages}"
+    await edit_or_reply(event, msg, link_preview=True)
