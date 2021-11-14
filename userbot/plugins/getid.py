d543790893d055e8464c6ceb612c6da641ddfc78
NekkoYato <89446086+NekkoYato@users.noreply.github.com>
2021-11-14 11:46:28 +0530

    Committing in accordance with the prophecy.

--- userbot/plugins/getid.py
+++ userbot/plugins/getid.py
@@ -1,4 +1,9 @@
-from telethon.utils import pack_bot_file_id
+# Modified by @kirito6969
+
+import html
+
+from telethon.tl.types import User
+from telethon.utils import get_display_name, pack_bot_file_id
 
 from userbot import catub
 from userbot.core.logger import logging
@@ -59,3 +64,47 @@ async def _(event):
 
     else:
         await edit_or_reply(event, f"**Current Chat ID : **`{event.chat_id}`")
+
+
+@catub.cat_cmd(
+    pattern="ids ?([\s\S]*)",
+    command=("ids", plugin_category),
+    info={
+        "header": "To get id of the group or user.",
+        "description": "If given input then shows id of that given chat/channel/user else if you reply to user then shows id of the replied user \
+    along with current chat id and if not replied to user or given input then just show id of the chat where you used the command",
+        "usage": "{tr}ids <reply/username>",
+    },
+)
+async def get_id(e):
+    custom = e.pattern_match.group(1)
+    if custom:
+        try:
+            custom = int(custom)
+        except ValueError:
+            pass
+        try:
+            en = await e.client.get_entity(custom)
+        except ValueError:
+            await edit_delete(e, "**Err** \\\nUnknown entity")
+            return
+        id = await e.client.get_peer_id(en)
+        text = html.escape(get_display_name(en))
+        if isinstance(en, User):
+            text = f'<a href="tg://user?id={id}">{text}</a>'
+        elif getattr(en, "username", None):
+            text = f'<a href="tg://resolve?domain={en.username}">{text}</a>'
+        text += f"'s ID: <code>{id}</code>"
+        await edit_or_reply(e, text, parse_mode="HTML")
+        return
+    text = f"<b>👥 Chat ID:</b> <code>{e.chat_id}</code>\n"
+    text += f"<b>💬 Message ID:</b> <code>{e.id}</code>\n"
+    text += f'<b>🙋‍♂️ Your ID:</b> <code>{e.sender_id}</code> • <a href="tg://user?id={e.sender_id}">Link</a>\n'
+    if e.is_reply:
+        text += "\n"
+        r = await e.get_reply_message()
+        text += f"<b>RepliedMessage ID:</b> <code>{r.id}</code>\n"
+        text += f'<b>RepliedSender ID:</b> <code>{r.sender_id}</code> • <a href="tg://user?id={r.sender_id}">Link</a>\n'
+        if getattr(r.fwd_from, "from_id", None):
+            text += f'<b>RepliedForward SenderID:</b> <code>-100{r.fwd_from.from_id.channel_id}</code> • <a href="tg://user?id=-100{r.fwd_from.from_id.channel_id}">Link</a>\n'
+    await edit_or_reply(e, text, parse_mode="HTML")
