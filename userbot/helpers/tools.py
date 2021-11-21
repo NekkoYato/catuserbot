09acec651796d298efb9c27656e408af39267596
NekkoYato <89446086+NekkoYato@users.noreply.github.com>
2021-11-22 00:33:52 +0530

    me nub sar

--- userbot/helpers/tools.py
+++ userbot/helpers/tools.py
@@ -23,7 +23,7 @@ def media_type(message):
 
 async def post_to_telegraph(page_title, html_format_content):
     post_client = TelegraphPoster(use_api=True)
-    auth_name = "CatUserbot"
+    auth_name = "Yato"
     post_client.create_api_token(auth_name)
     post_page = post_client.post(
         title=page_title,
